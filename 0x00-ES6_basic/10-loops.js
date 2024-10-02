74ca0af5edf68953c25d02b4f91f518434d7a48e
Tigo-cmd <ja16162040@gmail.com>
2024-10-02 19:07:56 +0100

    [Fix-Check] eslint-disable-next-line no-param-reassign

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,6 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   let i = 0;
   for (const idx of array) {
     const value = idx;
+    // eslint-disable-next-line no-param-reassign
     array[i] = appendString + value;
     i += 1;
   }
