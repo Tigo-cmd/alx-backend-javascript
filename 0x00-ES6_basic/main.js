2ee6aac3baac30c3f2105d0d5b9d386e925c93db
Tigo-cmd <ja16162040@gmail.com>
2024-10-02 11:44:48 +0100

    [Update] test 4-rest-parameter.js

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,5 +1,3 @@
-import getSumOfHoods from './3-default-parameter';
+import returnHowManyArguments from './4-rest-parameter';
 
-console.log(getSumOfHoods(34));
-console.log(getSumOfHoods(34, 3));
-console.log(getSumOfHoods(34, 3, 4));
+console.log(returnHowManyArguments('2523535'));
