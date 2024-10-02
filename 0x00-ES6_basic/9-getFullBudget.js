7cfefd7b1fe1ff7b6fae93732a9730c423ef74cb
Tigo-cmd <ja16162040@gmail.com>
2024-10-02 14:25:26 +0100

    [Fix] typo default function name

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,7 +1,7 @@
-import getBugetObject from './7-getBudgetObject';
+import getBudgetObject from './7-getBudgetObject';
 
-export default function getFullBujectObject(income, gdp, capita) {
-  const budget = getBugetObject(income, gdp, capita);
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
     getIncomeDollars(income) { return `$${income}`; },
