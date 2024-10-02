12ad42c13662fea19cf662604ac34c4467900188
Tigo-cmd <ja16162040@gmail.com>
2024-10-02 19:06:05 +0100

    [Add] utilizing the for..of ES6 statement

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
+  for (const idx of array) {
+    const value = idx;
+    array[i] = appendString + value;
+    i += 1;
+  }
+  return array;
+}
