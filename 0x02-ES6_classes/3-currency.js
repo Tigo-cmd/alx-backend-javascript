cd275d5cf4d8901bf46aa11fa12e19baa12174e6
Tigo-cmd <ja16162040@gmail.com>
2024-10-04 15:52:32 +0100

    [Add] Methods, static methods, computed methods names..... MONEY

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,39 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof name !== 'number') {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof code !== 'number') {
+      throw new TypeError('Code must be a string');
+    }
+    /* eslint-disable no-underscore-dangle */
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = value;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Code must be a string');
+    }
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
