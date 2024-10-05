4f7047f9305fe3f00f6e5a960c73f138bdb40f96
Tigo-cmd <ja16162040@gmail.com>
2024-10-05 01:43:38 +0100

    [Add] Class staatement as properties

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,42 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency = new Currency()) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency');
+    }
+
+    /* eslint-disable no-underscore-dangle */
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a string');
+    }
+    this._amount = value;
+  }
+
+  set currency(value = new Currency()) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be a Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this._currency._name} ${this._currency._code}`;
+  }
+}
