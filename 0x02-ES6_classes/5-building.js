8c5bbf0408051c4ad388aacf6883e3d65d09802f
Tigo-cmd <ja16162040@gmail.com>
2024-10-06 22:42:38 +0100

    [Fix] Eslint yellings

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,9 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
+    if (this.constructor === Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
     /* eslint-disable no-underscore-dangle */
     this._sqft = sqft;
