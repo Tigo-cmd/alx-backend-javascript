5a986fc1ab67420c5a1fff92a2d302c629c1e3d5
Tigo-cmd <ja16162040@gmail.com>
2024-10-06 23:27:03 +0100

    implemented type checking at first call function

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,7 +3,13 @@ import Building from './5-building';
 
 export default class skyHighBuilding extends Building {
   constructor(sqft, floors) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    } else if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
     super(sqft);
+    this._sqft = sqft;
     this._floors = floors;
   }
 
