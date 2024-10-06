af444956659cf1b1c34b37a7820590ba276d187a
Tigo-cmd <ja16162040@gmail.com>
2024-10-06 23:22:17 +0100

    [Add][Fix] Building evacuation implementation

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,8 +1,8 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+    // if (typeof this.evacuationWarningMessage !== 'function') {
+    //   throw new Error('Class extending Building must override evacuationWarningMessage');
+    // }
     /* eslint-disable no-underscore-dangle */
     this._sqft = sqft;
   }
@@ -14,12 +14,5 @@ export default class Building {
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-    this._sqft = value;
   }
 }
---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class skyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get floors() { return this._floors; }
+
+  get sqft() { return this._sqft; }
+
+  evacuationWarningMessage() { return `Evacuate slowly the ${this._floors} floors`; }
+}
