b9d35d79e05ae1111abe10cbd2ec6e05587d660a
Tigo-cmd <ja16162040@gmail.com>
2024-10-06 22:09:22 +0100

    [Fix] Class extending Building must override  evacuationWarningMessage warning

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,19 +1,18 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
+    // if (this.constructor === Building) {
+    //   throw new Error('Class extending Building must override evacuationWarningMessage');
+    // }
 
-    if (this.constructor === Building) {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
-
-    if (typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
-
+    // if (typeof this.evacuationWarningMessage !== 'function') {
+    //   throw new Error('Class extending Building must override evacuationWarningMessage');
+    // }
     /* eslint-disable no-underscore-dangle */
     this._sqft = sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage')
   }
 
   get sqft() {
