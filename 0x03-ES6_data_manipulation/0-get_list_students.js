2e33872b6382800efa3ec45d99b55853bf4b8bd7
Tigo-cmd <ja16162040@gmail.com>
2024-10-07 19:36:02 +0100

    [Add] array of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,19 @@
 export default function getListStudents() {
-
-
-    return [
+  return [
     {
       id: 1,
-        firstName: 'Guillaume',
-        location: 'San Francisco',
+      firstName: 'Guillaume',
+      location: 'San Francisco',
     },
     {
-        id: 2,
-        firstName: 'James',
-        location: 'Columbia',
+      id: 2,
+      firstName: 'James',
+      location: 'Columbia',
     },
     {
-        id: 3,
-        firstName: 'Serena',
-        location: 'San Francisco',
+      id: 3,
+      firstName: 'Serena',
+      location: 'San Francisco',
     },
-]
+  ];
 }
