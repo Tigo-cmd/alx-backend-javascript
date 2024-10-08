b7ec92b3f3c1065eb7113ba646a15f6753f07684
Tigo-cmd <ja16162040@gmail.com>
2024-10-08 01:15:39 +0100

    [Add]: Array.prototype.reduce

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,9 @@
+export default function getStudentIdsSum(students) {
+  if (!(students instanceof Array)) {
+    return [];
+  }
+
+  const studentsid = students.map((students) => students.id);
+  const initialValue = studentsid[0];
+  return studentsid.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue);
+}
