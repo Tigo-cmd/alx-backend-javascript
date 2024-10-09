369b915b0876d98132f941857f786558f9906300
Tigo-cmd <ja16162040@gmail.com>
2024-10-10 00:57:35 +0100

    Task 6: comlpleted

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,67 @@
+export interface DirectorInterface {
+  workFromHome(): string
+  getCoffeeBreak(): string
+  workDirectorTasks(): string
+}
+
+export interface TeacherInterface {
+  workFromHome(): string
+  getCoffeeBreak(): string
+  workTeacherTasks(): string
+}
+
+ export class Director implements DirectorInterface {
+
+  workFromHome() {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak() {
+    return 'Getting a coffe break';
+  }
+
+  workDirectorTasks() {
+    return 'Getting to director tasks';
+  }
+}
+ 
+
+export class Teacher implements TeacherInterface {
+
+  workFromHome() {
+    return 'Cannot Workfrom home';
+  }
+
+  getCoffeeBreak() {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks() {
+    return 'Getting to work';
+  }
+}
+
+
+export function createEmployee(salary: (number | string)): (DirectorInterface | TeacherInterface) {
+  if (typeof salary === 'number') {
+    if (salary < 500) {
+      return new Teacher;
+    }
+    else { return new Director; }
+  }
+  return new Director;
+ }
+
+
+ export function isDirector(employee: (Director | Teacher)) {
+  return employee instanceof Director;
+ }
+
+ export function executeWork(employee: (Director | Teacher)) {
+  if (isDirector(employee)) {
+   const director = new Director()
+   return director.workDirectorTasks();
+  } 
+  const teacher = new Teacher()
+  return teacher.workTeacherTasks();
+ }
