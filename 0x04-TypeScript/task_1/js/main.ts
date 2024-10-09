8ce17efd15c6744d866b03ebafae2092837b4229
Tigo-cmd <ja16162040@gmail.com>
2024-10-09 23:28:03 +0100

    [Updated] Writing a class and implement corresponding interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -12,6 +12,15 @@ export interface printTeacherFunction {
   lastName: string;
 }
 
+export interface studentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export interface studentClassSonctructor {
+  new (firstName: string, lastName: string): studentClass
+}
+
  export interface Directors extends Teacher {
   numberOfReports: number;
  }
@@ -20,3 +29,22 @@ export interface printTeacherFunction {
  export function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`;
  }
+
+ export class StudentClass implements studentClass {
+  private _firstName: string;
+  private _lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework() {
+    return 'Currently Working';
+  }
+
+  displayName() {
+    return this._firstName;
+  }
+ }
+ 
