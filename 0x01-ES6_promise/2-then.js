293bd8043e7659962b93e5b431426802006b4873
Tigo-cmd <ja16162040@gmail.com>
2024-10-14 00:27:46 +0100

    [Add] catch me if you can

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,14 @@
 export default function handleResponseFromAPI(promise) {
-  console.log('Got a response from the API');
   return promise
-    .then({ status: 200, body: 'success' })
-    .catch(new Error());
+    .then(() => {
+      console.log('Got a response from the API');
+     return { 
+        status: 200, 
+        body: 'success' 
+      };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+     return new Error();
+    });
 }
