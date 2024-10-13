fcb35597e513d081ee8a0bb9097f0bee859278f0
Tigo-cmd <ja16162040@gmail.com>
2024-10-14 00:02:57 +0100

    [Fix] lint complaint

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,7 +2,8 @@ export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({status: 200, body: 'Success'});
-    } else {
+    } 
+    clearelse {
       reject(new Error('The fake API is not working currently'));
     }
   });
