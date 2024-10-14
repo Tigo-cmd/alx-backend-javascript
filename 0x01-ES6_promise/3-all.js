cd5e49216fa8e1fdf8f9bee847d901873c633690
Tigo-cmd <ja16162040@gmail.com>
2024-10-14 14:07:31 +0100

    [Add]  Handle multiple successful promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,6 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto, createUser]).then((values) => {
-    console.log(values);
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    for (let item of values) {
+      for (const [key, value] of Object.entries(item)) {
+        console.log(value);
+      }
+    }
   }).catch(new Error('Signup system offline'));
 }
