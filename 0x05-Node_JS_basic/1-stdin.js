b5ff1921f0ac4810b97d410bc52f3eeb3c06712a
Tigo-cmd <ja16162040@gmail.com>
2024-11-27 22:34:52 +0100

    [Add]

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,15 @@
 // 1-stdin.js
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf-8');
 
-// Listen for user input
 process.stdin.on('data', (input) => {
-  const name = input.trim(); // Remove trailing newlines or spaces
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  process.stdin.end(); // End the input stream
+  const name = input.trim(); // Remove extra spaces and newlines
+  process.stdout.write(`Your name is: ${name}\r`);
+  process.stdout.write('This important software is now closing\n');
+  process.stdin.end();
 });
 
-// Handle process end event
 process.stdin.on('end', () => {
   process.exit(0);
 });
