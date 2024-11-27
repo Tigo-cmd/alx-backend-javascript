2e93d94912b890d06a632dc9dfd9ebe49e322473
Tigo-cmd <ja16162040@gmail.com>
2024-11-27 10:45:21 +0100

    [Fix]ix] used another approach with the prcesss module

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,28 @@
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+// readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
+//   console.log(`Your name is: ${name}`);
+//   readline.close();
+// });
+
+/*
+*
+* i decided to use a new approach to this regarding ALX requirements
+*/
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-readline.question('Welcome to Holberton School, what is your name?\n', (name) => {
-  console.log(`Your name is: ${name}`);
-  readline.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing');
 });
