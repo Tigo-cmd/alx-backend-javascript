51081046f4eeefbb6382efaef4c4a6de30a5f961
Tigo-cmd <ja16162040@gmail.com>
2025-01-26 23:12:00 +0100

    [fix][Update][Add] finished app

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
-    const item1 = values[0];
-    const item2 = values[1];
-    console.log(item1.body, item2.firstName, item2.lastName);
-  }).catch(console.log('Signup system offline'));
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
